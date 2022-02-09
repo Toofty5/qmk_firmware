50df205a7c16af1ef40e89d07ce302c4a3265039
Toofty5 <jay.cheung@gmail.com>
2022-02-09 18:21:50 -0500

    Fixed column layout

--- keyboards/handwired/holyoak/config.h
+++ keyboards/handwired/holyoak/config.h
@@ -22,8 +22,8 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    %MANUFACTURER%
-#define PRODUCT         S60-X RGB
+#define MANUFACTURER    Toofty5
+#define PRODUCT         Holy Oak
 
 /* key matrix size */
 #define MATRIX_ROWS 9
@@ -40,7 +40,7 @@
  *
  */
 #define MATRIX_ROW_PINS { D3, D2, D1, D0, D4, C6, D7, E6, B4 }
-#define MATRIX_COL_PINS { B5, F4, F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { B5, B6, B2, B3, B1, F7, F6, F5, F4 }
 
 #define DIODE_DIRECTION COL2ROW
 
